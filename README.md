6afd277745d51334d3dde29112f98498da52f6ae
Teddy Park <tg911022@naver.com>
2021-06-22 21:38:08 +0900

    done

---
+++ README.md
@@ -0,0 +1,91 @@
+# 간단한 웹브라우저
+
+### WebView
+~~~kotlin
+// 인터넷 권한 설정 in AdroidMenifest.xml
+<uses-permission android:name="android.permission.INTERNET"/>
+<application android:usesCleartextTraffic="true"> // http 허용
+// in MainActivity.kt
+// 기본적으로 안드로이드의 default 웹브라우저(chrome)로 실행 됨
+// 현재 webView에서 보려면 webViewClient를 덮어씀
+webView.webViewClient = WebViewClient()
+webView.settings.javaScriptEnabled = true // webView에서 JS 동작
+webView.loadUrl("url") // webView url 페이지 실행
+~~~
+
+### setOnEditorActionListener, IME_ACTION
+IME_ACTION_DONE : 키보드 엔터버튼 입력 시 키보드가 내려감
+~~~kotlin
+// in activity_main.xml
+<EditText
+    android:imeOptions="actionDone" // 키보드 엔터버튼을 정의
+    android:inputType="textUri"/> // 키보드 유형을 정의
+// in MainActivity.kt
+addressText.setOnEditorActionListener { v, actionId, event ->
+	// IME_ACTION_DONE 일 경우 url 페이지 실행
+    if (actionId == EditorInfo.IME_ACTION_DONE) {
+        webView.loadUrl(v.text.toString())
+    }
+    return@setOnEditorActionListener false
+~~~
+
+### onBackPressed : 안드로이드 backButton 정의
+~~~kotlin
+// in MainActivity.kt
+override fun onBackPressed() {
+    if(webView.canGoBack()) // webView 히스토리가 남아있다면 종료X
+        webView.goBack() // 뒤로가기
+    else
+        super.onBackPressed() // 프로세스 종료
+    }
+~~~
+
+### Swiperefreshlayout : 화면을 아래로 드래그하여 새로고침
+~~~kotlin
+// in build.gradle
+dependencies {
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+}
+
+// in activity_main.xml // SwipeRefreshLayout 안에 WebView 정의
+<androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+	android:id="@+id/refreshLayout"
+	android:layout_width="0dp"
+	android:layout_height="0dp"
+	app:layout_constraintBottom_toBottomOf="parent"
+	app:layout_constraintEnd_toEndOf="parent"
+	app:layout_constraintStart_toStartOf="parent"
+	app:layout_constraintTop_toBottomOf="@id/toolbar">
+	<WebView
+		android:id="@+id/webView"
+		android:layout_width="match_parent"
+		android:layout_height="match_parent" />
+</androidx.swiperefreshlayout.widget.SwipeRefreshLayout>
+
+// in MainActivity.kt
+// 페이지 로딩이 완료되면 Refresh 버튼을 제거
+inner class MyWebViewClient: WebViewClient() {
+	override fun onPageFinished(view: WebView?, url: String?) {
+		super.onPageFinished(view, url)
+
+		refreshLayout.isRefreshing = false
+	}
+}
+~~~
+
+### ContentLoadingProgressBar
+~~~kotlin
+// in activity_main.xml
+<androidx.core.widget.ContentLoadingProgressBar
+	style="@style/Widget.AppCompat.ProgressBar.Horizontal"/>
+
+// in MainActivity.kt
+// WebView 의 프로그레스 변화를 progressBar 에 적용
+inner class MyWebChromeClient: WebChromeClient() {
+	override fun onProgressChanged(view: WebView?, newProgress: Int) {
+		super.onProgressChanged(view, newProgress)
+
+		progressBar.progress = newProgress
+	}
+}
+~~~
--- app/src/main/java/com/example/mini_webbrowser/MainActivity.kt
+++ app/src/main/java/com/example/mini_webbrowser/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.mini_webbrowser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById<ContentLoadingProgressBar>(R.id.progressBar)
+    }
 
     private val homeButton: ImageButton by lazy {
         findViewById<ImageButton>(R.id.homeButton)
@@ -27,6 +36,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<EditText>(R.id.addressBar)
     }
 
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+    }
+
     private val webView: WebView by lazy {
         findViewById<WebView>(R.id.webView)
     }
@@ -37,6 +50,8 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         bindViews()
+        initButtons()
+        refreshEvent()
     }
 
 
@@ -44,21 +59,87 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews() {
         webView.apply {
-            webViewClient = WebViewClient()
+            webViewClient = MyWebViewClient()
+            webChromeClient = MyWebChromeClient()
             settings.javaScriptEnabled = true
-            loadUrl("https://www.google.com")
+            loadUrl(DEFAULT_URL)
         }
     }
 
     private fun bindViews() {
         addressText.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(v.text.toString())
-            }
+                val loadingUrl = v.text.toString()
 
+                if (URLUtil.isNetworkUrl(loadingUrl))
+                    webView.loadUrl(loadingUrl)
+                else
+                    webView.loadUrl("http://${loadingUrl}")
+            }
             return@setOnEditorActionListener false
         }
     }
 
+    override fun onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
 
+    private fun initButtons() {
+        backButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        forwardButton.setOnClickListener {
+            webView.goForward()
+        }
+
+        homeButton.setOnClickListener {
+            webView.loadUrl(DEFAULT_URL)
+        }
+    }
+
+    private fun refreshEvent() {
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+
+    inner class MyWebViewClient: WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            if (webView.canGoBack())
+                backButton.isEnabled
+            if (webView.canGoForward())
+                forwardButton.isEnabled
+
+            addressText.setText(url)
+
+        }
+    }
+
+    inner class MyWebChromeClient: WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_URL = "https://www.google.com"
+    }
 }
